8e896871ed3015cba7d4d5edd2f969d6cadb8c03
Vlad-Leskov <148856733+Vlad-Leskov@users.noreply.github.com>
2024-07-28 13:22:22 +0300

    1

--- src/validation/models/contact.js
+++ src/validation/models/contact.js
@@ -1,3 +1,4 @@
+import Joi from 'joi';
 import { model, Schema } from 'mongoose';
 
 const contactSchema = new Schema(
@@ -28,3 +29,21 @@ const contactSchema = new Schema(
 );
 const Contact = model('contacts', contactSchema);
 export default Contact;
+
+export const validateContact = Joi.object({
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().min(3).max(20).required(),
+  email: Joi.string().email(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
+});
+
+export const validateUpdate = Joi.object({
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().min(3).max(20),
+  email: Joi.string().email(),
+  ifFavourite: Joi.boolean(),
+  contactType: Joi.string()
+    .valid('work', 'home', 'personal')
+    .default('personal'),
+});
